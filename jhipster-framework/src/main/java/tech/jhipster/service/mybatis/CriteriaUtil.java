373d0951792139ce3940740201f72ec6f66e958f
wangxin <wangxinxx@163.com>
2025-01-05 08:04:40 +0800

    add CriteriaUtil method

--- jhipster-framework/src/main/java/tech/jhipster/service/mybatis/CriteriaUtil.java
+++ jhipster-framework/src/main/java/tech/jhipster/service/mybatis/CriteriaUtil.java
@@ -1,12 +1,16 @@
 package tech.jhipster.service.mybatis;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.diboot.core.binding.query.BindQuery;
 import tech.jhipster.service.Criteria;
+import tech.jhipster.service.filter.Filter;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class CriteriaUtil {
@@ -36,5 +40,35 @@ public class CriteriaUtil {
             e.printStackTrace();
         }
         return nonNullFields;
+    }
+
+    public static Map<String, Map<String, Object>> getNonIgnoredAndNonNullFields(Criteria criteria) {
+        // 将对象转换为 Map，包含所有字段及其值
+        Map<String, Object> fieldMap = BeanUtil.beanToMap(criteria, false, true);
+        Map<String, Map<String, Object>> result = new HashMap<>();
+        fieldMap.forEach((key, value) -> {
+            try {
+                if (!(value instanceof Filter)) {
+                    return;
+                }
+                Field field = criteria.getClass().getDeclaredField(key);
+                field.setAccessible(true);
+
+                // 检查字段是否有 @BindQuery 注解
+                if (field.isAnnotationPresent(BindQuery.class)) {
+                    BindQuery bindQuery = field.getAnnotation(BindQuery.class);
+                    // 检查 @BindQuery 注解的 ignore 值，并且值非空
+                    if (!bindQuery.ignore()) {
+                        Map<String, Object> fieldTypeMap = new HashMap<>();
+                        fieldTypeMap.put("value", value);
+                        fieldTypeMap.put("bindQuery", bindQuery);
+                        result.put(key, fieldTypeMap);
+                    }
+                }
+            } catch (NoSuchFieldException e) {
+                // 忽略异常，继续处理其他字段
+            }
+        });
+        return result;
     }
 }
